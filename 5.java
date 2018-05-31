2ad539aa0c28b4a02a6b01f4dc298297e2645bc0
sagar-suman <sagarsumn@gmail.com>
2018-05-31 18:10:48 +0530

    2nd Commit

---
+++ 5.java
@@ -0,0 +1,7 @@
+import java.io.*
+        BufferedReaderInput=new BufferedReader(new InputStreamReader(System.in));
+
+            void main()
+                {
+            System.out.print("Demo");
+        }
